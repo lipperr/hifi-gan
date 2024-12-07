38f94bc12af69b962758b29808274bb0038b2f16
lipperr <hcivokr@gmail.com>
2024-12-07 22:01:00 +0300

    resolved issues with resampler

--- src/metrics/mos/mos.py
+++ src/metrics/mos/mos.py
@@ -10,12 +10,15 @@ class MOS(BaseMetric):
         path = os.path.join(os.path.expanduser('~'), ".cache/wv_mos/wv_mos.ckpt")
         self.model =  Wav2Vec2MOS(path)
         self.resample = torchaudio.transforms.Resample(orig_freq= 22050, new_freq= 16000)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.resample.to(self.device)
         super(MOS, self).__init__()
 
     def __call__(self, audio_pred, **kwargs):
         metric = 0
         for wav in audio_pred:
-            out = self.resample(wav.squeeze(0))
+            audio = torch.clone(wav).squeeze(0)
+            out = self.resample(audio)
             metric += self.model(out)
         metric /= audio_pred.shape[0]
         return metric.item()
